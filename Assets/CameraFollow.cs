8b0bb0c817e955f0940d6f8e8044f48886480ca0
Igor Bessa <ibessa@ucsc.edu>
2024-06-10 17:55:38 -0700

    bounded camera to maze

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -9,6 +9,10 @@ public class CameraFollow : MonoBehaviour
     public Vector2 offset;
     private Vector2 velocity = Vector2.zero;
 
+    public Vector2 minPos;
+    public Vector2 maxPos;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +25,8 @@ public class CameraFollow : MonoBehaviour
         if(target != null){
             Vector2 targetPos = ( new Vector2(target.position.x, target.position.y)) + offset;
             Vector2 xyPos = Vector2.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+            xyPos.x = Mathf.Clamp(xyPos.x, minPos.x, maxPos.x);
+            xyPos.y = Mathf.Clamp(xyPos.y, minPos.y, maxPos.y);
 
             transform.position = new Vector3(xyPos.x, xyPos.y, -50);
 
