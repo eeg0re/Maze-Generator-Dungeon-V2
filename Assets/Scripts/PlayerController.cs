1546ac6f9f2946d31f93149aa50e8989fb80fe5c
Igor Bessa <ibessa@ucsc.edu>
2024-06-21 11:28:14 -0300

    removed demo video from repo to avoid problems

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,7 +29,7 @@ public class PlayerController : MonoBehaviour
                         stateLock = true;
                         canMove = false;
                         break;
-            }
+                }
             }
             
         }
